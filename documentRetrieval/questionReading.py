821dca6de928f1f4518089e6b5b9a4e4e544b069
landjbs <landonsmith@college.harvard.edu>
2019-07-24 13:34:43 +0100

    questionReading parser updates

--- documentRetrieval/questionReading.py
+++ documentRetrieval/questionReading.py
@@ -17,17 +17,20 @@ PATH = 'data/inData/natural_questions/v1.0/train'
 # matcher for header tags in html text
 headerMatcher = re.compile('^h[1-6$]')
 # matcher for p tags in html text
-pMatcher = re.compile('^p')
+pMatcher = re.compile('^P')
 
 
-def analyze_wiki_html(rawHTML):
+def clean_wiki_html(rawHTML):
     """ Helper to convert raw wiki html to a clean div dict """
     # create soup object for the html string
     soupObj = BeautifulSoup(rawHTML, 'html.parser')
     # find title
     title = soupObj.title.text
     # find raw text of page
-    rawText = " ".join(str(p) for p in soupObj.find_all(pMatcher, text=True))
+    # rawText = " ".join(str(p) for p in soupObj.find_all(pMatcher))
+    rawText = " ".join(re.findall(r'(?<=<P>)[^</P>]+(?=</P>)',
+                                    string=rawHTML,
+                                    flags=re.IGNORECASE))
     return {'title':title,
                 'text': clean_web_text(rawText)}
 
@@ -37,16 +40,28 @@ def read_question_dataset(path):
     Reads Google Natural Questions dataset into dataframe
         -path: the path to the folder containing jsonl files to analyze
     """
-    files = listdir(path)
-    for file in files:
+
+    def scrape_wiki_file(file):
+        """ Helper pulls information out of wiki file and returns dict """
         if file.endswith('.jsonl'):
             with open(f'{path}/{file}', 'r') as questionFile:
                 for i, questionDict in enumerate(json_lines.reader(questionFile)):
                     # get question text and vectorize
                     questionText = questionDict['question_text']
-                    questionVec = docVecs.vectorize_doc(questionText)
+                    # questionVec = docVecs.vectorize_doc(questionText)
+
                     # get dict of page divisions and vectorize
-                    divDict = analyze_wiki_html(questionDict['document_html'])
+                    divDict = clean_wiki_html(questionDict['document_html'])
+                    # divVecs = {divName:(docVecs.vectorize_doc(divText))
+                    #             for divName, divText in divDict.items()}
+
+                    # get list of start locations for each long answer candidate
                     longAnswerCandidates = questionDict['long_answer_candidates']
                     longAnswerStarts = [candidate['start_token']
                                         for candidate in longAnswerCandidates]
+                    if divDict['text']=="": print(questionDict['document_html'])
+
+    # scrape files in
+    infoList = [scrape_wiki_file(file) for file in listdir(path)]
+    dataframe = pd.DataFrame(infoList)
+    return dataframe
