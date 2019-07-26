8ae7dcddc6d7fd5171f6a4ce6489aa87a65a9325
landjbs <landonsmith@college.harvard.edu>
2019-07-26 13:09:58 +0100

    reading path changes

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-# from questionAnswering.answer import  answer_question
-
-# while True:
-#     text = input('Text: ')
-#     question = input("Question: ")
-#     print(answer_question(question, text))
-
-
 # from documentRetrieval.questionReading import read_question_dataset
 # PATH = 'data/inData/natural_questions/v1.0/train'
 # x = read_question_dataset(PATH, n=2500, outPath='data/outData/questionsDf2500.sav')
 
 from questionAnswering.questionAnalysis import build_question_database
 PATH = 'data/inData/natural_questions/v1.0/train'
-x = build_question_database(PATH, n=2, outPath='data/outData/answeringDf1.sav')
+x = build_question_database(PATH, n=2500, outPath='data/outData/answeringDf1.sav')
