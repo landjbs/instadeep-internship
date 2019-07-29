89800db7247ead08ec81a8f1302166fe8eaf6762
landjbs <landonsmith@college.harvard.edu>
2019-07-29 21:13:34 +0100

    reading progress

--- questionAnswering/squadAnalysis.py
+++ questionAnswering/squadAnalysis.py
@@ -4,24 +4,30 @@ import numpy as np
 from tqdm import tqdm
 
 def clean_text(text):
-    return re.sub(r'e.g.', "", text)
+    decimalSub = re.sub(r'(?<=.)\.(?=.)', ',', text)
+    return re.sub(r'e.g.', "", decimalSub)
+
 
 answerMetrics = []
 with open('data/inData/train-v2.0.json') as squadFile:
     for categorty in (json.load(squadFile)['data']):
         for paragraph in categorty['paragraphs']:
             paragraphText = clean_text(paragraph['context'])
-            paraSents = re.split(r'[.?!]', paragraphText)
+            paraSents = [sent.strip().lower()
+                        for sent in re.split(r'[.?!]', paragraphText)
+                        if not sent==""]
             for qas in paragraph['qas']:
-                question = qas['question']
+                question = qas['question'].lower()
                 answerList = qas['answers']
                 if not answerList==[]:
-                    answerText = answerList[0]['text']
+                    answerText = clean_text(answerList[0]['text'].lower())
                     isIn = False
                     for sent in paraSents:
                         if answerText in sent:
                             isIn = True
+                            numIn += 1
                     if not isIn:
-                        print(f'{answerText}\n{paraSents}')
+                        # print(f'{answerText}\n{paraSents}')
+                        notIn += 1
                 else:
                     pass
