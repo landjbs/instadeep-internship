7265a69047fc34da4fc804b240a1362806217115
landjbs <landonsmith@college.harvard.edu>
2019-07-30 20:36:52 +0100

    pytorchModel init

---
+++ questionAnswering/pytorchModel.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from bert_serving.client import BertClient
+
+bc = BertClient(check_length=True)
+
+torch.manual_seed(1)
+
+
+raw_data = [
+    ("How are you? I am well".lower(), [0,0,0,0,0,1]),
+    ("Who are you? I am me".lower(), [0,0,0,0,0,1]),
+    ("What are you? I am me".lower(), [0,0,0,1,1,0])
+]
+
+training_data = []
+
+#### CURRENTLY TAKES FIRST WORD VECTOR FOR TESTING———NEEDS UPDATE #####
+for dataPoint in raw_data:
+    sentence = dataPoint[0]
+    sentenceVec = [num for num in bc.encode([sentence])[0][1]]
+    training_data.append((torch.tensor(sentenceVec), dataPoint[1]))
+
+
+tag_to_ix = {0: 0, 1: 1}
+
+
+# These will usually be more like 32 or 64 dimensional.
+# We will keep them small, so we can see how the weights change as we train.
+EMBEDDING_DIM = 6
+HIDDEN_DIM = 30
+
+
+class LSTMTagger(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # print(self.word_embeddings)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
+
+
+model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, 1024, len(tag_to_ix))
+
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+# See what the scores are before training
+# Note that element i,j of the output is the score for tag j for word i.
+# Here we don't need to train, so the code is wrapped in torch.no_grad()
+# with torch.no_grad():
+#     inputs = prepare_sequence(training_data[0][0], word_to_ix)
+#     tag_scores = model(inputs)
+#     print(tag_scores)
+
+for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+    for sentence, tags in training_data:
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Step 2. Get our inputs ready for the network, that is, turn them into
+        # Tensors of word indices.
+        # sentence_in = prepare_sequence(sentence, word_to_ix)
+        sentence_in = sentence
+
+        # targets = prepare_sequence(tags, tag_to_ix)
+        targets = tags
+
+        # Step 3. Run our forward pass.
+        tag_scores = model(sentence_in)
+
+        # Step 4. Compute the loss, gradients, and update the parameters by
+        #  calling optimizer.step()
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+
+
+
+# See what the scores are after training
+# with torch.no_grad():
+#     inputs = prepare_sequence(('what are you? i am me').split(), word_to_ix) # training_data[0][0]
+#     tag_scores = model(inputs)
+#
+#     # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
+#     # for word i. The predicted tag is the maximum scoring tag.
+#     # Here, we can see the predicted sequence below is 0 1 2 0 1
+#     # since 0 is index of the maximum value of row 1,
+#     # 1 is the index of maximum value of row 2, etc.
+#     # Which is DET NOUN VERB DET NOUN, the correct sequence!
+#     print(f"{'-'*40}\n{tag_scores}\n{'-'*40}")
+#     plt.imshow(tag_scores)
+#     plt.show()
