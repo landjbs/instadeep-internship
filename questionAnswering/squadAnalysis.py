cf3a593b72e9be30a8e3d767c5588abdee8c5d55
landjbs <landonsmith@college.harvard.edu>
2019-07-30 14:12:29 +0100

    squad concat init

--- questionAnswering/squadAnalysis.py
+++ questionAnswering/squadAnalysis.py
@@ -8,11 +8,11 @@ from bert_serving.client import BertClient
 
 bc = BertClient(check_length=True)
 
-answerMetrics = []
+dataList = []
 with open('data/inData/train-v2.0.json') as squadFile:
     for categorty in json.load(squadFile)['data']:
         print(f"Category: {categorty['title']}")
-        
+
         for paragraph in (categorty['paragraphs']):
             paragraphText = paragraph['context'].lower()
 
@@ -20,19 +20,26 @@ with open('data/inData/train-v2.0.json') as squadFile:
             tokenizedSents = [word_tokenize(sent) for sent in sentences]
             sentVecs = bc.encode(tokenizedSents, is_tokenized=True)
 
-            wordEmbeddings = []
             for sentNum, wordVecs in enumerate(sentVecs):
                 for wordNum, wordVec in enumerate(wordVecs):
                     if not (wordVec[0]==0):
                         curWord = tokenizedSents[sentNum][wordNum-1]
                         wordEmbeddings.append((curWord, wordVec))
 
+            wordEmbeddings = np.array(wordEmbeddings)
+
+            # scoring (wordVec, inAnswerSpan)
             for qas in paragraph['qas']:
                 question = re.sub("\\?", "", qas['question'].lower())
                 questionWords = word_tokenize(question)
-                questionEmbeddings = [wordVec for wordVec
+                questionEmbeddings = np.array([(0, wordVec) for wordVec
                                         in bc.encode([questionWords], is_tokenized=True)[0]
-                                        if not (wordVec[0]==0)]
+                                        if not (wordVec[0]==0)])
+
+                wordEmbeddings[:, 0] = np.zeros(wordEmbeddings.shape[0])
+                print(wordEmbeddings.shape)
+                data = np.concatenate(questionEmbeddings, wordEmbeddings)
+                print(data)
 
                 # print(f'questionVec: {len(questionVec)}')
                 answerList = qas['answers']
@@ -40,5 +47,5 @@ with open('data/inData/train-v2.0.json') as squadFile:
                     answerTokens = None
                 else:
                     answerText = answerList[0]['text'].lower()
-                    answerStart = answerList[0]['answer_start']
-                    answerSpan = (answerStart, (answerStart + (len(answerText))))
+                    answerWords = word_tokenize(answerText)
+                    answerLen = len(answerWords)
