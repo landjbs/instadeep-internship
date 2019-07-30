562635d26971ed4c1784f239a2451433a257385b
landjbs <landonsmith@college.harvard.edu>
2019-07-30 20:56:21 +0100

    new tokenization model

---
+++ test.py
@@ -0,0 +1,15 @@
+from bert_serving.client import BertClient
+from nltk.tokenize import word_tokenize
+
+
+bc = BertClient(check_length=True)
+
+while True:
+    sent = input('sent: ')
+    tokens = word_tokenize(sent)
+    sentVec = bc.encode([tokens], is_tokenized=True)
+    sentVec = list(filter(lambda l:l[0]!=0, sentVec[0]))
+    print([(tokens[i], wordVec) for i, wordVec in enumerate(sentVec)])
+
+
+# import questionAnswering.squadAnalysis
