0713276d3da9c6cb983ae11fd5a2ec0baccfa2af
landjbs <landonsmith@college.harvard.edu>
2019-07-31 15:44:36 +0100

    kerasModel rewrite init

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ read_squad_dataset(squadPath='data/inData/train-v2.0.json',
                     paraDepth=1,
                     picklePath='data/outData/squadDataFrame.sav',
                     csvPath='data/outData/squadDataFrameBACKUP.csv')
+
+# import questionAnswering.kerasModel
