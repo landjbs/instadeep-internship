e0160d22cf4eb7cf26c12b67936ff64b0bb85543
landjbs <landonsmith@college.harvard.edu>
2019-07-31 10:55:12 +0100

    keras model init

---
+++ questionAnswering/kerasModel.py
@@ -0,0 +1,14 @@
+class KerasBatchGenerator(object):
+
+    def __init__(self, data, num_steps, batch_size, vocabulary, skip_step=5):
+        self.data = data
+        self.num_steps = num_steps
+        self.batch_size = batch_size
+        self.vocabulary = vocabulary
+        # this will track the progress of the batches sequentially through the
+        # data set - once the data reaches the end of the data set it will reset
+        # back to zero
+        self.current_idx = 0
+        # skip_step is the number of words which will be skipped before the next
+        # batch is skimmed from the data set
+        self.skip_step = skip_step
